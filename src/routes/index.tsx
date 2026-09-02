b168ff18c719b5a98932eda6e5aec45dd2ead3aa
silenthill99 <florian.graziani@sfr.fr>
2026-09-02 12:26:02 +0200

    Mise à jour du projet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,7 +15,7 @@ function Home() {
   const todos = Route.useLoaderData()
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+      <h1 className="text-4xl font-bold">Welcome to TanStack Start12345</h1>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
