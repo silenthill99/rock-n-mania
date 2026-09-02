537292098995ee2c952d9c324ff8e981ad7d1692
silenthill99 <florian.graziani@sfr.fr>
2026-09-02 11:30:00 +0200

    Mise à jour du projet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,25 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+import { prisma } from '#/db.ts'
 
-export const Route = createFileRoute('/')({ component: Home })
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: () => getTodos(),
+})
+
+const getTodos = createServerFn({ method: 'GET' }).handler(() =>
+  prisma.todo.findMany(),
+)
 
 function Home() {
+  const todos = Route.useLoaderData()
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
+      <p>{todos.length}</p>
     </div>
   )
 }
